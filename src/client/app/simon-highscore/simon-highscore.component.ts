1f762ba6cfcb431c5b7e34d7c39e93f5b3fc2798
Tom Teman <tomteman42@gmail.com>
2016-05-05 11:41:08 -0600

    game metrics

--- src/client/app/simon-highscore/simon-highscore.component.ts
+++ src/client/app/simon-highscore/simon-highscore.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from 'angular2/core';
 import {Observable} from 'rxjs/Observable';
 
+
 @Component({
   moduleId: __moduleName,
   selector: 'simon-highscore',
@@ -10,10 +11,35 @@ import {Observable} from 'rxjs/Observable';
 export class SimonHighscoreComponent implements OnInit {
   @Input() games: Observable<ISimonScoreInfo[]>;
   private highscore: number;
+  private gameMetrics = {};
+  private gameColors: string[] = ['red', 'green', 'blue', 'yellow'];
+
+  generateTeamsMetrics(games) {
+    for (let color of this.gameColors) {
+      let high: number = 0;
+      let sum: number = 0;
+      let avg: string = "0.0";
+
+      const teamScores = games.filter(game => game.color === color).map(teamGame => teamGame.score);
+      if (teamScores.length) {
+        high = Math.max.apply(Math, teamScores);
+        sum = teamScores.reduce((a, b) =>  a + b );
+        avg = (sum / teamScores.length).toFixed(1);
+      }
+
+      this.gameMetrics[color] = {
+        high: high,
+        sum: sum,
+        avg: avg,
+      }
+    }
+
+  }
 
   ngOnInit() {
     this.games.subscribe(games => {
       this.highscore = 0;
+      this.generateTeamsMetrics(games);
       games.forEach(game => {
         if (game.score > this.highscore) {
           this.highscore = game.score;
